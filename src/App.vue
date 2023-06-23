7a361f2c69a22146fd98b9bfe72e7e6048b0dd83
blackbrokkoli <kastomat@gmail.com>
2023-06-23 12:08:11 +0200

    allow getting all notes with certain tag

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-    <h2>Hello,Developer!</h2>
-    <Hello></Hello>
-    <Hi :hi="hi"></Hi>
-    <HiHi></HiHi>
+	<h2>Hello,Developer!</h2>
 </template>
 
 <script setup lang="tsx">
@@ -10,13 +7,30 @@ import Hello from "./Hello";
 import Hi from "./Hi.vue";
 import { ref } from "vue";
 let hi = ref("");
-let HiHi = () => (<h1><Hello></Hello></h1>)
+let HiHi = () => (
+	<h1>
+		<Hello></Hello>
+	</h1>
+);
 
+// get all Obsidian notes with tag #vr
+const allNotesWithTag = app.vault.getMarkdownFiles().filter((note) => {
+	let willBeIncluded = false;
+	const tags = this.app.metadataCache.getFileCache(note)?.tags;
+	if (tags) {
+		if (tags.filter((tag) => tag.tag === "#ux-vr").length > 0) {
+			willBeIncluded = true;
+		}
+	}
+	return willBeIncluded;
+});
+// pick 30 randomly
+const notes = allNotesWithTag.sort(() => Math.random() - Math.random()).slice(0, 30);
+console.log(notes);
 </script>
-
 
 <style scoped>
 h2 {
-    color: lightcoral;
+	color: lightcoral;
 }
 </style>
